6b5c649bba074d3b4cdb2cddf3def66f0452588c
Lucas Vergara Ibañez <lucas.ivergara18@gmail.com>
2019-11-08 14:47:04 -0300

    login
    php

---
+++ Fromiti/prueba.php
@@ -0,0 +1,16 @@
+#Prueba de un login en HTML y PHP como prueba
+<html>
+    <head>    
+    </head>
+    <body>
+        <form>
+            Usuario: <input type="text" name="user"><br>
+            Contraseña: <input type="password" name="pwd"><br>
+            <input type="submit" value = "Entrar">
+        </form>        
+    </body>
+</html>
+
+
+
+    
