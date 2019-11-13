4527350c7f915bf6320bf91fe2a67e7873c8668b
Lucas Vergara Ibañez <56010205+Fromiti@users.noreply.github.com>
2019-11-13 17:16:12 -0300

    Login casi terminado

--- APP/PHP and MySql/login.php
+++ APP/PHP and MySql/login.php
@@ -1,49 +1,28 @@
 <?php
-
-	$hostname='localhost';
+    $hostname='localhost';
     $database='id11498467_registro_de_usuarios';
     $username='id11498467_root';
     $password='nokia303..';
-	
-	$json=array();
-		if(isset($_POST["correo"]) && isset($_POST["contrasena"])){
-			
-			$user=$_POST['correo'];
-			$pwd=$_POST['contrasena'];
-			
-			//$user = "lucas.ivergara18@gmail.com";
-			//$pwd = "nokia303..";
-			
-			$conexion=mysqli_connect($hostname,$username,$password,$database);
-		
-			$consulta="SELECT correo, contrasena, nombres, edad, apellidos FROM usuarios WHERE correo = '{$user}' AND contrasena = '{$pwd}'";
-			
-			$resultado=mysqli_query($conexion,$consulta);
 
-			if($consulta){
-				if($reg=mysqli_fetch_array($resultado)){
-					$json['datos'][]=$reg;
-				}
-				mysqli_close($conexion);
-				echo json_encode($json);
-			
-			}else{	
-				$results["correo"]='';
-				$results["contrasena"]='';
-				$results["nombres"]='';
-				$results["apellidos"]='';
-				$results["edad"]='';
-				$json['datos'][]=$results;
-			echo json_encode($json);
-			}
-		
-		}else{
-			$results["correo"]='';
-			$results["contrasena"]='';
-			$results["nombres"]='';
-			$results["apellidos"]='';
-			$results["edad"]='';
-			$json['datos'][]=$results;
-			echo json_encode($json);
-		}
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
+    $con = mysqli_connect($hostname, $username, $password, $database);
+
+    $email = $_POST['correo'];
+    $password = $_POST['contrasena'];
+
+    $Sql_Query = "select * from usuarios where correo = '$email' and contrasena = '$password' ";
+
+    $check = mysqli_fetch_array(mysqli_query($con,$Sql_Query));
+
+        if(isset($check)){
+            echo "True";
+        }else{
+            echo "False";
+        }
+}else{
+    echo "Check Again";
+    }
+    mysqli_close($con);
+
 ?>
